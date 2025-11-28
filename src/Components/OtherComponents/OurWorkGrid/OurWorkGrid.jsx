480aa1bf94319c3e9c8cf9286e2dc9f2b00e6d21
webtesting22 <developer.bliss@gmail.com>
2025-11-28 16:48:54 +0530

    Updated code

--- src/Components/OtherComponents/OurWorkGrid/OurWorkGrid.jsx
+++ src/Components/OtherComponents/OurWorkGrid/OurWorkGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, {  useEffect, useMemo, useState } from "react";
 import { createPortal } from "react-dom";
 import "./OurWorkGrid.css";
 import CenteredHeader from "../../CommonUsedComponents/CenteredHeader/CenteredHeader";
@@ -60,6 +60,13 @@ const OurWorkGrid = () => {
         const videoId = extractYouTubeId(url);
         return videoId ? `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` : "";
     };
+
+useEffect(() => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}, []);
 
     const videoModal = activeVideo
         ? createPortal(
